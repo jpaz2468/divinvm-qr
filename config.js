a03d6d5e5bc20de6279a80761cb4b2762a1dfd0f
jpaz2468 <jpaz.magic@gmail.com>
2026-04-19 03:35:30 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.DIVINVM_CONFIG = {
+  SUPABASE_URL: "https://tcszavmrfcdlzuobagdz.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_MIhPNq0UdGUI58Wyk87O5w_Gfs83v4m",
+  ADMIN_SECRET: "1234"
+};
